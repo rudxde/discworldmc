dc2ef1f3da72f0407a5768a8d2384c492c9262c0
rudxde <kj4derechte@gmail.com>
2024-05-19 13:43:27 +0200

    feat: filter allowed servers based on focused value in DiscordCommandsManager

--- src/discord/commands/index.ts
+++ src/discord/commands/index.ts
@@ -178,7 +178,12 @@ export class DiscordCommandsManager {
     private async autoCompleteServer(interaction: AutocompleteInteraction, action: PossibleActions): Promise<void> {
         const memberRoles = await this.getMemberRoles(interaction);
         const allowedServers = this.minecraftServerProvider.getAllowedServerInfosForPermissions(memberRoles, action);
-        await interaction.respond(allowedServers.map(server => ({
+        const focusedValue = interaction.options.getFocused();
+        let filteredServers = allowedServers;
+        if (focusedValue) {
+            filteredServers = allowedServers.filter(server => server.id.includes(focusedValue));
+        }
+        await interaction.respond(filteredServers.map(server => ({
             name: server.id,
             value: `#${server.id}`,
         })));
